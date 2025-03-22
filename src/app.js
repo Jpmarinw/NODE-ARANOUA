a40a5ca2f8a502aa9fe6609bf13193ec00562392
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2025-03-22 09:24:54 -0400

    Criação das rotas de CRUD e Banco de dados

--- src/app.js
+++ src/app.js
@@ -5,57 +5,65 @@ const app = express();
 app.use(express.json());
 
 const cidades = [
-    {
-        "id": 1,
-        "nome": "Manaus",
-        "uf": "AM"
-    },
-    {
-        "id": 2,
-        "nome": "Rio de Janeiro",
-        "uf": "RJ"
-    },
-    {
-        "id": 3,
-        "nome": "São Paulo",
-        "uf": "SP"
-    }
-]
+  {
+    id: 1,
+    nome: "Manaus",
+    uf: "AM",
+  },
+  {
+    id: 2,
+    nome: "Rio de Janeiro",
+    uf: "RJ",
+  },
+  {
+    id: 3,
+    nome: "São Paulo",
+    uf: "SP",
+  },
+];
 
 app.get("/", (req, res) => {
-    res.status(200).send("Home - Express");
+  res.status(200).send("Home - Express");
 });
 
 app.get("/cidades", (req, res) => {
-
-    res.status(200).send(cidades);
+  res.status(200).send(cidades);
 });
 
 app.get("/cidades/:id", (req, res) => {
-
-    const id = req.params.id;
-
-    const cidade = cidades.find(c => c.id == id);
-
+  const id = req.params.id;
+  const cidade = cidades.find((elemento) => elemento.id === Number(id)) || null;
+  if (cidade) {
     res.status(200).send(cidade);
-})
+  } else {
+    res.status(404).send("Cidade não encontrada");
+  }
+});
 
 app.post("/cidades", (req, res) => {
-    const cidade = req.body;
-    cidades.push(cidade);
-    res.status(201).send(cidade);
+  cidades.push(req.body);
+  res.status(201).send(req.body);
 });
 
 app.put("/cidades/:id", (req, res) => {
-    const nome = req.body;
-
+  const id = req.params.id;
+  const cidade = cidades.find((elemento) => elemento.id === Number(id)) || null;
+  cidade.nome = req.body.nome;
+  cidade.uf = req.body.uf;
+  res.status(200).send("cidade alterada com sucesso");
 });
 
+function deletarCidade(id) {
+  const indice = cidades.findIndex((elemento) => elemento.id === Number(id));
+  if (indice !== -1) {
+    const cidadesRemovidas = cidades.splice(indice, 1);
+    return cidadesRemovidas[0];
+  }
+  return null;
+}
 app.delete("/cidades/:id", (req, res) => {
-    const id = req.params.id;
-    const index = cidades.findIndex(c => c.id == id);
-    cidades.splice(index, 1);
-    res.status(204).send();
+  const cidadeDeletada = deletarCidade(req.params.id);
+  res.status(200).send("Cidade Deletada com sucesso");
 });
 
 export default app;
