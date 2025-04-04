20262696834f43df830c90cced8fdef004ba5f3e
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2025-04-04 10:59:08 -0400

    Criando rotas de get e post

--- aula07/src/controllers/PessoaController.js
+++ aula07/src/controllers/PessoaController.js
@@ -8,3 +8,12 @@ export const listarPessoas = async (req, res) => {
         res.status(500).json({ error: 'Erro ao listar pessoas' });
     }
 };
+
+export const criarPessoa = async (req, res) => {
+    try {
+        const novaPessoa = await Pessoa.create(req.body);
+        res.status(201).json(novaPessoa);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar pessoa', detalhes: error});
+    }
+};
--- aula07/src/routers/PessoaRouter.js
+++ aula07/src/routers/PessoaRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import {
-    listarPessoas
+    listarPessoas,
+    criarPessoa
 } from "../controllers/PessoaController.js";
 
 const router = express.Router();
 
 router.get('/', listarPessoas);
+router.post('/', criarPessoa);
 
 export default router;
