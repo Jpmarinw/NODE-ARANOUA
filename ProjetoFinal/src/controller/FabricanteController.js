3a7b564dc1a8dabc4b6fe20ef01fbe400377cffe
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2025-04-22 21:12:14 -0400

    Criando os critérios de aceitação de POST de Fabricantes

--- ProjetoFinal/src/controller/FabricanteController.js
+++ ProjetoFinal/src/controller/FabricanteController.js
@@ -14,6 +14,30 @@ export const listarFabricantes = async (req, res) => {
 //Cadastra um novo fabricante
 export const criarFabricante = async (req, res) => {
     try {
+        const erros = [];
+        if (!req.body.nome || req.body.nome.trim() === "") {
+            erros.push("O campo nome é obrigatório.");
+        }
+        if (
+            !req.body.documento_registro ||
+            req.body.documento_registro.trim() === ""
+        ) {
+            erros.push("O campo documento de registro é obrigatório.");
+        }
+        if (!req.body.pais || req.body.pais.trim() === "") {
+            erros.push("O campo país é obrigatório.");
+        }
+        if (erros.length > 0) {
+            return res.status(400).json({ erros });
+        }
+        const registroExistente = await Fabricante.findOne({
+            where: { documento_registro: req.body.documento_registro },
+        });
+        if (registroExistente) {
+            return res.status(400).json({
+                error: "Já existe um fabricante cadastrado com esse documento de registro",
+            });
+        }
         const { nome, documento_registro, pais } = req.body;
         const novoFabricante = await Fabricante.create({
             nome,
